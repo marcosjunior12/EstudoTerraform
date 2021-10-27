cddcf9862b8b9f5dfc03917384be8d085ffbaab9
Marcos Junior <juninho1_2010@hotmail.com>
2021-10-26 21:09:52 -0300

    Learning Terraform

---
+++ 01-terraform-basico/01-primeiro-script/main.tf
@@ -0,0 +1,25 @@
+terraform {
+    required_version = "1.0.9"
+
+    required_providers {
+      aws = {
+          source = "hashicorp/aws"
+          version = "3.63.0"
+      }
+    }
+}
+
+provider "aws" {
+    region = "us-east-1"  
+}
+
+resource "aws_s3_bucket" "my-test-bucket" {
+  bucket = "my-tf-test-bucket-mjr12"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+    Managedby = "Terraform"
+  }
+}
---
+++ 01-terraform-basico/02-alterando-destruindo/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = "1.0.9"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.63.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "my-test-bucket" {
+  bucket = "my-tf-test-bucket-mjr1212"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+    Managedby   = "Terraform"
+    Owner       = "Marcos"
+    UpdateAt    = "26/10/2021"
+  }
+}
