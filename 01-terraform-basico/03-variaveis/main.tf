df2ffb6726ddc6dd6527d38a742536d5db7683eb
Marcos Junior <juninho1_2010@hotmail.com>
2021-10-28 19:36:25 -0300

    Variables terraform

---
+++ 01-terraform-basico/03-variaveis/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = "1.0.9"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.63.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+resource "aws_instance" "web" {
+  ami           = var.instance_ami
+  instance_type = var.instance_type
+
+  tags = var.instance_tags
+}
---
+++ 01-terraform-basico/03-variaveis/terraform.tfvars
@@ -0,0 +1,5 @@
+aws_region = "sa-east-1"
+
+instance_ami = "ami-05855ed85de7fbd77"
+
+instance_type = "t2.micro"
